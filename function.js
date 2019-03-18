a6b910341a868b9fb8c60dcaaf7f2523f1377f1e
Hemant Singh <info90.hemant@gmail.com>
2019-03-18 17:19:04 +0530

    function concept added

---
+++ function.js
@@ -0,0 +1,12 @@
+function myFunc(theObject) {
+    theObject.companyName = 'BMW';
+}
+  
+var mycar = { companyName: 'Honda', model: 'BMW i8', Price: '2.62 crore'};
+var x, y;
+  
+x = mycar.companyName; // x gets the value "Honda"
+console.log(x)
+myFunc(mycar);
+y = mycar.companyName; // y gets the value "BMW"
+console.log(y);
