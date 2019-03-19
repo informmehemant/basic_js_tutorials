43ea0e117daf5092f2fa74411b17ed9cbf755624
Hemant Singh <info90.hemant@gmail.com>
2019-03-19 16:45:13 +0530

    more closure code is added

--- functionExample/closures.js
+++ functionExample/closures.js
@@ -1,3 +1,33 @@
+// first Class function
+var myFunction = function() {
+    console.log(' i have been called');
+}
+myFunction.firstName = 'Hemant';
+console.log(myFunction);
+// self executing function
+// (function(){
+//     var message = ['hello',' hi'];
+//     for(var i in message) {
+//         setTimeout(function() {
+//             console.log(message[i]);
+//         },10);
+//     }  
+// })();
+
+(function(){
+    var message = ['hello',' hi'];
+    // capture context
+    var say = function(index) { // establishes new scope its almost creating a new varible
+        return function() {
+            console.log(message[index]);
+        };
+    }
+    for(var i in message) {
+       setTimeout(say(i), 1000);
+    }  
+})();
+
+
 var counter = 0;
 function createCounter(x) { 
     return function() {
@@ -6,3 +36,4 @@ function createCounter(x) {
 }
 var c = createCounter(2);
 console.log(c());
+
