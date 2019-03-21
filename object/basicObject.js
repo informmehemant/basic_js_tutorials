5a1fc26db953feb68d27464c6f62334d20c72b4d
Hemant Singh <info90.hemant@gmail.com>
2019-03-21 11:14:09 +0530

    some more details object eg added

--- object/basicObject.js
+++ object/basicObject.js
@@ -1,14 +1,24 @@
 // basic object discription
-
+// json way
 var person = {
     name: 'Rahul',
     email: 'rahul123@gmail.com',
     address: {
         street: '24th main, norkil hotel',
         state: 'sikkim'
+    },
+    printDetail: function() {
+        console.log(this.address);
     }
 }
 // access Objects
 console.log(person.email);
 console.log(person.address.state);
+// accessing function too
+console.log(person.printDetail());
 
+// declaring using new key word
+var person1 = new Object();
+person1.name = 'Shivam saxena';
+
+console.log(person1);
