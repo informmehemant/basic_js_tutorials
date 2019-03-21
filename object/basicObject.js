602bb627fcd8c0fc21f46cf79cd8199750d76131
Hemant Singh <info90.hemant@gmail.com>
2019-03-21 11:08:24 +0530

    object example added

---
+++ object/basicObject.js
@@ -0,0 +1,14 @@
+// basic object discription
+
+var person = {
+    name: 'Rahul',
+    email: 'rahul123@gmail.com',
+    address: {
+        street: '24th main, norkil hotel',
+        state: 'sikkim'
+    }
+}
+// access Objects
+console.log(person.email);
+console.log(person.address.state);
+
